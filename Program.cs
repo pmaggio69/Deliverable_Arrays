ed9b945658f3797872028c3e205f60fe7baf772a
maggi <maggi@WINDOWS-LRUR2K3>
2023-02-08 23:59:50 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Author: Patrick
+// date: 2/8/23
+// Description: create a program to write Fibonacci numbers
+namespace Deliverable_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            int[] fibonacci = new int[x];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for(int i = 0; i < x; i++)
+            {
+                if(i >= 2)
+                {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+            }
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("Fibonacci(" + i + ") = " + fibonacci[i]);
+            }
+        }
+    }
+}
